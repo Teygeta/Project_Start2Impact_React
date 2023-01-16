64031b74c98ae26ff7e6069e5bbd3e30504bb37d
Teygeta <vitto.gioda@outlook.it>
2023-01-16 15:55:15 +0100

    Corrected final imperfections to prepare for deployment

--- src/components/InfoWindow.jsx
+++ src/components/InfoWindow.jsx
@@ -22,11 +22,11 @@ export function InfoWindow() {
           classNames="info-window"
           unmountOnExit
         >
-          <div className="info-window">
+          <div className="info-window w-5/6">
             <button onClick={handleShowHideModal} className="mx-2 absolute right-4">X</button>
             <h1 className="font-extrabold mb-5 text-2xl">App usage:</h1>
             1. Set the timer <br /><br />
-            2. Choose the sounds you want  <br />(sounds can be overlaid)<br /> <br />
+            2. Choose the sounds you want  <br />(sounds can be overlaid and you can double-click on the volume indicator to reset the volume)<br /> <br />
             3. Start the timer and enjoy the meditation
             <div className="italic font-light text-sm m-5">A sound will be played at the end of the time. The music will
               not be interrupted; you choose how quietly to end it</div>
--- src/components/VolumeRange.jsx
+++ src/components/VolumeRange.jsx
@@ -40,25 +40,31 @@ export function VolumeRange() {
           audio.volume = e.target.value
         }
 
+        //reset volume value on double-click
+        const handleResetVolume = (e) => {
+          e.target.value = 1
+        }
+
         return (
           <div key={index} className="md:m-10 m-3 text-xl w-60">
             {item.name}
 
             <label className="swap ml-2 relative bottom-[-7px]">
-              <input type="checkbox" onClick={handlePlayPause} />
+              <input type="checkbox" onClick={handlePlayPause}/>
               <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="28" height="28"
-                viewBox="0 0 24 24">
+                   viewBox="0 0 24 24">
                 <path
-                  d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z" />
+                  d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"/>
               </svg>
               <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="28" height="28"
-                viewBox="0 0 24 24">
+                   viewBox="0 0 24 24">
                 <path
-                  d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z" />
+                  d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"/>
               </svg>
             </label>
-            <input type="range" min="0" max="1" step="0.01" className="range range-xs md:range-sm "
-              onChange={handleChangeVolume} />
+            <input type="range" min="0" max="1" step="0.01" className="range range-xs md:range-sm"
+                   onChange={handleChangeVolume}
+                   onDoubleClick={handleResetVolume}/>
           </div>
         )
       })}
